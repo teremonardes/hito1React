6bcd0376e7afd085e4b3fc75184fd8eb662b53f6
Tere Monardes <teremonardes.s@gmail.com>
2024-11-01 15:31:34 -0300

    hito1

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+import Home from './components/Home'
+import Footer from './components/Footer'
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Navbar from './components/Navbar'
+
+function App() {
+  return (
+    <>
+    <Navbar></Navbar>
+    <Home></Home>
+    <Footer></Footer>
+
+    </>
+  )
+}
+
+export default App
